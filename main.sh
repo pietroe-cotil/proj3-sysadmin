6aa6c9cafac5c91667c95d5651aa5eda45e060c9
pietro <cl205231@g.unicamp.br>
2025-11-26 00:21:03 +0000

    Finalização

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ read -p "Qual diretório você gostaria de identificar? " dir
 
 if [ -d "$dir" ]; then
     echo "Diretório encontrado."
-    usoporc=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')8
+    usoporc=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
     if [ "$usoporc" -gt "$valcritico" ]; then
         echo "[CRÍTICO]: Uso de disco acima de $valcritico%! ($usoporc%)"
@@ -18,7 +18,16 @@ if [ -d "$dir" ]; then
         echo "Uso de disco normal! ($usoporc%)"
     fi
 
+    quantiaProcessos=$(ps -ef -u $USER | wc -l)
 
+    echo "O usuário atual está rodando $quantiaProcessos processos."
+    echo -e "\nProcessos que mais utilizam memória:\n"
+    echo "PID | Comando"
+    echo "--------------"
+ 
+    ps aux -u $USER --sort=-%mem --noheaders | head -n 5 | awk '{print $2 " - " $11}' | while read -r line; do
+        echo "$line"
+    done
 else
     echo "Esse diretório não existe."
     exit 1
